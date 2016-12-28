d948968939ad086b5a558577f5e2145597125bb7
kartik <kartikaggarwal98@gmail.com>
2016-12-28 14:31:53 +0530

    added superuser

--- app/views.py
+++ app/views.py
@@ -16,3 +16,8 @@ import random
 def index(request):
 	context_dict={}
 	return render(request,'home.html',context_dict)
+
+def meets():
+	context_dict={}
+	return render(request,'calendar.html',context_dict)
+
